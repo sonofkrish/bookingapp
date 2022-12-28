a3695cd8622db1dbd0c1f3ed24b5ff0384205597
Balasubramani K <sonofkrishna.k@gmail.com>
2022-12-28 22:53:07 +0530

    react ui added

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -31,7 +31,7 @@ export const login = async (req, res, next) => {
     );
 
     if (!isPasswordCorrect)
-      return createError(400, "Wrong password or username!");
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,23 +5,26 @@ import {
   deleteHotel,
   getHotel,
   getHotels,
+  countByCity,
 } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 
 //DELETE
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 // GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
 
 export default router;
